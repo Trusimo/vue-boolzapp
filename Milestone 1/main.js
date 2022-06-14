864573b34184527738271afe1b2ed5961a649b7b
Trusimo <simonetrunfio96@gmail.com>
2022-06-14 18:52:51 +0200

    code completed (besides img not working)

--- Milestone 1/main.js
+++ Milestone 1/main.js
@@ -1,8 +1,42 @@
 const appVue = new Vue({
-    
+
     el: "#app",
 
     data: {
+        users: [
+            {
+                img: "img/avatar_1.jpg",
+                name: "Michele",
+                message: "Ultimo messaggio inviato"
+            },
+            {
+                name: "Fabio",
+                message: "Ultimo messaggio inviato"
+            },
+            {
+                name: "Samuele",
+                message: "Ultimo messaggio inviato"
+            },
+            {
+                name: "Alessandro B.",
+                message: "Ultimo messaggio inviato"
+            },{
+                name: "Alessandro L.",
+                message: "Ultimo messaggio inviato"
+            },
+            {
+                name: "Claudia",
+                message: "Ultimo messaggio inviato"
+            },
+            {
+                name: "Federico",
+                message: "Ultimo messaggio inviato"
+            },
+            {
+                name: "Davide",
+                message: "Ultimo messaggio inviato"
+            },
+        ]
 
 },
 
